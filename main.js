7c4d5320aca88b4800621e7b2a9004961f7c2222
martian17 <yakumodanshi@gmail.com>
2022-01-14 14:37:42 +0900

    keyboard reorganization

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ let keyres = ()=>{};
 window.addEventListener("keydown",function(e){
     //console.log(e);
     if(e.ctrlKey)return false;
-    if(e.key === "ArrowLeft"){
+    if(e.key === "ArrowLeft" || e.key === "ArrowUp" || e.key === "a" || e.key === "w"){
         keyres("L");
-    }else if(e.key === "ArrowRight"){
+    }else if(e.key === "ArrowRight" || e.key === "ArrowDown" || e.key === "d" || e.key === "s"){
         keyres("R");
+    }else if(e.key === " "){
+        keyres("S");
     }
 });
 
@@ -16,6 +18,50 @@ const keyEvt = function(){
         keyres = res;
     });
 };
+
+/*const waitKey = function(){
+    let arr = [...arguments];
+    return new Promise((res,rej)=>{
+        let evtfunc = function(e){
+            //console.log(e);
+            if(e.ctrlKey)return false;
+            if(arr.indexOf(e.key) === -1)return false;
+            window.removeEventListener("keydown",evtfunc);
+            res(e.key);
+        };
+        window.addEventListener("keydown",evtfunc);
+    });
+};*/
+
+let Keys = new (function(){
+    let cbs = {};
+    window.addEventListener("keydown",function(e){
+        let key = e.key;
+        if(!(key in cbs))return;
+        cbs[key].map(cb=>{
+            cb(e);
+        });
+    });
+    this.on = function(key,cb){
+        cbs[key] = cbs[key] || [];
+        cbs[key].push(cb);
+        return {
+            remove:()=>{
+                cbs[key].splice(cbs[key].indexOf(cb),1);
+            }
+        }
+    };
+})();
+
+let spaceEvt = (()=>{
+    return {
+        remove:()=>{
+            
+        }
+    };
+})();
+
+
 
 let parseTime = function(t){
     t = t.split(":").map(a=>parseInt(a));
@@ -73,7 +119,6 @@ let setCursorPosition = async function(elem,i){
 
 let pauser = new Pauser();
 
-
 let main = async function(){
     let body = new ELEM(document.body);
     let BG = body.add("div","class:bg");
@@ -83,6 +128,7 @@ let main = async function(){
     let right = body.add("div","class:right");
     let schedE = right.add("div","class:sched");
     let labels = [];
+    let STATE = "";
     for(let i = 0; i < stages.length; i++){
         let {label,time,img} = stages[i];
         let l = schedE.add("div",0,label+formatTimeJP(time));
@@ -90,31 +136,42 @@ let main = async function(){
     }
     
     //edit time functionality
-    (()=>{
-        let clickfn = async (e)=>{
-            await pauser.pause();//await ensures the break happens at the call point
-            timeE.attr("contenteditable",true);
-            
-            //focusing, and setting the caret position
-            let elem = timeE.e;
-            //e.focus();
-            setCursorPosition(elem,elem.innerHTML.length);
-            
-            
-            let keyEvt = timeE.on("keydown",(e)=>{
-                let time1 = parseTime(timeE.e.innerHTML);
-                if(e.key === "Enter"){
-                    keyEvt.remove();
-                    pauser.resume(time1,Date.now());
-                    timeE.attr("contenteditable",false);
-                    timeE.once("click",clickfn);
-                }
-            });
+    let clickfn = async (e)=>{
+        if(STATE !== "SEQUENCE"){
+            timeE.once("click",clickfn);
+            return;
         }
-        timeE.once("click",clickfn);
-    })();
+        let [time,now] = await pauser.pause();//await ensures the break happens at the call point, ensuring the execution order
+        timeE.attr("contenteditable",true);
+        
+        //focusing, and setting the caret position
+        let elem = timeE.e;
+        //e.focus();
+        setCursorPosition(elem,elem.innerHTML.length);
+        
+        let enterEvt = timeE.on("keydown",(e)=>{
+            if(e.key === "Enter"){
+                time = parseTime(timeE.e.innerHTML);
+                resume();
+            }
+        });
+        
+        let spaceEvt = Keys.on(" ",(e)=>{
+            time = parseTime(timeE.e.innerHTML);
+            resume();
+        });
+        
+        let resume = function(){
+            enterEvt.remove();
+            spaceEvt.remove();
+            pauser.resume(time,Date.now());
+            timeE.attr("contenteditable",false);
+            timeE.once("click",clickfn);
+        }
+    }
+    timeE.once("click",clickfn);
     
-    let i = 0;
+    let i = -1;
     
     //contains the major sequence
     let sequence = async function(){
@@ -126,8 +183,13 @@ let main = async function(){
             let start = Date.now();
             while(true){
                 let now = Date.now();
-                let elapsed = Math.floor(time-(now-start)/1000);
-                timeE.setInner(formatTime(elapsed));
+                let remaining = Math.floor(time-(now-start)/1000);
+                if(remaining <= 10){
+                    timeE.style("color:#F00;");
+                }else{
+                    timeE.style("color:;");
+                }
+                timeE.setInner(formatTime(remaining));
                 
                 //edits the time if paused
                 [time,start] = await pauser.wait(time,start);
@@ -137,6 +199,10 @@ let main = async function(){
                     break;
                 }else if(v === "R"){
                     break;
+                }else if(v === "S"){
+                    //implementation in progress
+                    //console.log("asdfasd");
+                    //clickfn();
                 }else if((now-start)/1000 > time){
                     break;
                 }
@@ -150,17 +216,23 @@ let main = async function(){
     }
     
     while(true){
-        label1.setInner("--");
         timeE.setInner("0:00");
-        await keyEvt();
-        await sequence();
-        console.log(i);
+        label1.style("font-size:5vw;color:#aaa;");
         if(i < 0){
+            label1.setInner("↓キーで開始");
+            let key = await keyEvt();
+            if(key !== "R")continue;
             i = 0;
         }else if(i >= stages.length){
-            console.log("asdfasd");
+            label1.setInner("↑キーで戻る");
+            let key = await keyEvt();
+            if(key !== "L")continue;
             i = stages.length-1;
         }
+        label1.style("font-size:;color:;");
+        STATE = "SEQUENCE";
+        await sequence();
+        STATE = "";
     }
     
 };
